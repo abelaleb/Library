2c682ed2e07730c06e5b8b361c1680b1d2881198
abel <abelalebachewasfaw.gmail.com>
2024-04-17 11:25:49 +0300

    Add booktile and style bookitems

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 const myLibrary = [];
+let currentBook;
+let title, author;
 
+function book(title, author) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+}
 const form = document.getElementById("bookForm");
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   const bookTitle = document.getElementById("title").value;
   const bookAuthor = document.getElementById("author").value;
-  
-  if(bookTitle.trim() !== "" && bookAuthor.trim() !== ""){
-  const book = {
-    title: bookTitle,
-    author: bookAuthor,
-  };
-  myLibrary.push(book);
-  addBookToLibrary();
-  form.reset();
-  }
-  else{
-    alert("Please provide both title and author for the book.")
+
+  if (bookTitle.trim() !== "" && bookAuthor.trim() !== "") {
+    const books = new book(bookTitle, bookAuthor);
+    myLibrary.push(books);
+    addBookToLibrary();
+    form.reset();
+  } else {
+    alert("Please provide both title and author for the book.");
   }
 });
 
@@ -24,14 +27,22 @@ function addBookToLibrary() {
   const bookList = document.getElementById("book-list");
   bookList.innerHTML = "";
   myLibrary.forEach((book) => {
-    const bookItem = document.createElement("div");
-    bookItem.textContent = `${book.title} by ${book.author}`;
-    bookList.appendChild(bookItem);
+    const bookTile = document.createElement("div");
+    bookTile.classList.add("bookItem");
+    const bookTitle = document.createElement("div");
+    bookTitle.classList.add("bookTitle");
+    bookTitle.textContent = `"${book.title}"`;
+    const bookAuthor = document.createElement("div");
+    bookAuthor.classList.add("bookAuthor");
+    bookAuthor.textContent = `${book.author}`;
+    const read = document.createElement("button");
+    read.classList.add("read");
+    read.textContent = `read`;
+    const remove = document.createElement("button");
+    remove.classList.add("remove");
+    remove.textContent = `remove`;
+    bookTile.append(bookTitle,bookAuthor,read,remove)
+    bookList.appendChild(bookTile);
+    // bookItem.textContent = `Book: ${books.title} Author: ${books.author}`;
   });
 }
-
-function Book() {
-  // the constructor...
-}
-
-console.log(myLibrary);
